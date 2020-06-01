ed95d27929657e947b6db8a5b7cc028567808c1d
Alex Sparrow <alex@quartic.io>
2020-06-01 13:07:06 +0100

    Add comments and improve playground

--- sigurd-playground/components/SigurdRunner.tsx
+++ sigurd-playground/components/SigurdRunner.tsx
@@ -30,36 +30,76 @@ fn main(args: string) {
 `;
 
 const SigurdRunner = ({ rustModule }) => {
+  const [visibleTab, setVisibleTab] = useState("code");
   const [code, setCode] = useState(INITIAL_CODE);
   const [output, setOutput] = useState("");
   const [ast, setAst] = useState({});
 
   return (
-    <div style={{ width: "80vw", display: "flex", height: "80vh", maxHeight: "80vh" }}>
-      <div style={{ flex: 1 }}>
-        <AceEditor
-          mode="rust"
-          theme="github"
-          onChange={setCode}
-          value={code}
-          fontSize={18}
-          name="example"
-          style={{ height: "100%", width: "100%" }}
-          editorProps={{ $blockScrolling: true }}
-        />
+    <div
+      style={{
+        width: "100%",
+        paddingLeft: "1rem",
+        paddingRight: "1rem",
+        height: "70vh",
+        maxHeight: "70vh",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <div className="level is-mobile" style={{ flexGrow: 0, marginBottom: "5px" }}>
+        <div className="level-left tabs">
+          <ul>
+            <li className={visibleTab == "code" ? "is-active" : ""}>
+              <a onClick={() => setVisibleTab("code")}>Code</a>
+            </li>
+            <li className={visibleTab == "ast" ? "is-active" : ""}>
+              <a onClick={() => setVisibleTab("ast")}>AST</a>
+            </li>
+          </ul>
+        </div>
+        <div className="level-right">
+          <button
+            className="button is-primary is-small"
+            onClick={(e) => {
+              setAst(rustModule.parse(code));
+              setOutput(rustModule.run(code));
+            }}
+          >
+            Run
+          </button>
+        </div>
       </div>
-
-      <div style={{ flex: 1, overflow: "auto" }}>
-        <pre>{output}</pre>
-        <button
-          onClick={(e) => {
-            setAst(rustModule.parse(code));
-            setOutput(rustModule.run(code));
-          }}
-        >
-          Go
-        </button>
-        <ReactJson src={ast} />
+      <div style={{ flex: 1, maxHeight: "calc(100% - 41px)"}}>
+        {visibleTab == "code" ? (
+          <>
+            <AceEditor
+              mode="rust"
+              theme="github"
+              onChange={setCode}
+              value={code}
+              fontSize={18}
+              name="example"
+              style={{ height: "100%", width: "100%", overflow: "auto" }}
+              editorProps={{ $blockScrolling: true }}
+            />
+            <div><pre>
+              {output}
+              </pre></div>
+          </>
+        ) : (
+          <>
+            <ReactJson
+              style={{
+                width: "100%",
+                height: "100%",
+                overflow: "scroll",
+                maxHeight: "100%",
+              }}
+              src={ast}
+            />
+          </>
+        )}
       </div>
     </div>
   );
